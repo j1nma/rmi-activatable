03ae1a9699f077927a4f8edb07f41dba2a482839
j1nma <j1nma@users.noreply.github.com>
2018-09-12 21:24:52 -0300

    ClientInitiator.

--- client/src/main/java/rmi/client/ClientInitiator.java
+++ client/src/main/java/rmi/client/ClientInitiator.java
@@ -2,13 +2,37 @@ package rmi.client;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import rmi.CrowdfundingInitiatiorService;
+import rmi.Reward;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientInitiator {
 	private static Logger logger = LoggerFactory.getLogger(ClientInitiator.class);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
 		logger.info("Client Initiator Starting ...");
 		
+		final CrowdfundingInitiatiorService initiatiorService = (CrowdfundingInitiatiorService) Naming.lookup("//localhost:1099/InitiatorService");
+		
+		
+		for (int i = 0; i < 4; i++) {
+			
+			List<Reward> rewards = new ArrayList<>();
+			
+			rewards.add(new Reward("Ball", i + 1));
+			rewards.add(new Reward("Bat", i * i + 2));
+			rewards.add(new Reward("Hat", i * i * i + 3));
+			
+			initiatiorService.createProject("Project " + i, i * i, rewards);
+		}
+		
+		initiatiorService.cancelProject("Project 2");
 		
 	}
 }
